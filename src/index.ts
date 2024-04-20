78d9f221446b4e1ccad45935b4d614821e66f787
Takumikimura1899 <kimura141899@gmail.com>
2024-04-20 09:24:45 +0900

    /posts:id 実装

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const app = new Hono();
 
 let blogPosts = [
   { id: '1', title: 'First Post', content: 'This is my first post' },
-  { id: '1', title: 'First Post', content: 'This is my first post' },
-  { id: '1', title: 'First Post', content: 'This is my first post' },
+  { id: '2', title: 'First Post', content: 'This is my first post' },
+  { id: '3', title: 'First Post', content: 'This is my first post' },
 ];
 
 app.use('*', prettyJSON());
@@ -16,5 +16,14 @@ app.get('/', (c) => {
 });
 
 app.get('/posts', (c) => c.json({ posts: blogPosts }));
+
+app.get('/posts/:id', (c) => {
+  const id = c.req.param('id');
+  const post = blogPosts.find((p) => p.id === id);
+  if (!post) {
+    return c.json({ error: 'not found post' }, 404);
+  }
+  return c.json({ post });
+});
 
 export default app;
